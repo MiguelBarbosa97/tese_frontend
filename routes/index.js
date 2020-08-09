e56fa7eaf07b63ef485b675a561a377af69a9d5b
Miguel <miguelacosteirab97@gmail.com>
2020-08-09 15:25:28 +0100

    commit ui

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
-var multer  = require('multer')
-var crypto  = require('crypto')
+var multer = require('multer')
+var crypto = require('crypto')
 var mime = require("mime");
 
 var storagejs = multer.diskStorage({
@@ -28,66 +28,72 @@ var storagecss = multer.diskStorage({
 });
 
 var uploadcss = multer({ storage: storagecss });
-var router = express.Router() 
+var router = express.Router()
 
 router.post('/uploadLibJs', uploadjs.single('file'), function (req, res, next) {
-  res.send({filename : req.file.filename});
+  res.send({ filename: req.file.filename });
 })
 router.post('/uploadLibCss', uploadcss.single('file'), function (req, res, next) {
-  res.send({filename : req.file.filename});
+  res.send({ filename: req.file.filename });
 })
 
-router.get('/', function(req, res) {
+router.get('/getGeoData', function (req, res) {
+  var json = require('../public/geoJsonMap.json'); //with path
+  res.send(json);
+});
+
+
+router.get('/', function (req, res) {
   res.render('pages/login', { layout: 'login_layout' })
 })
 
-router.get('/home', function(req, res, next) {
+router.get('/home', function (req, res, next) {
   res.render('pages/home', { title: 'home' })
 })
 
-router.get('/connection', function(req, res, next) {
+router.get('/connection', function (req, res, next) {
   res.render('pages/connection', { title: 'connection' })
 })
 
-router.get('/query', function(req, res, next) {
+router.get('/query', function (req, res, next) {
   res.render('pages/query', { title: 'query' })
 })
 
-router.get('/visualization', function(req, res, next) {
+router.get('/visualization', function (req, res, next) {
   res.render('pages/visualization', { title: 'visualization' })
 })
 
-router.get('/dataVisualizations', function(req, res, next) {
+router.get('/dataVisualizations', function (req, res, next) {
   res.render('pages/datavisualization', { title: 'visualization' })
 })
-router.get('/libraries', function(req, res, next) {
+router.get('/libraries', function (req, res, next) {
   res.render('pages/libraries', { title: 'libraries' })
 })
 
-router.get('/marketplace', function(req, res, next) {
+router.get('/marketplace', function (req, res, next) {
   res.render('pages/marketplace', { title: 'marketplace' })
 })
-router.get('/dashboards', function(req, res, next) {
+router.get('/dashboards', function (req, res, next) {
   res.render('pages/dashboardFolder', { title: 'dashboards' })
 })
 
-router.get('/dashboard', function(req, res, next) {
+router.get('/dashboard', function (req, res, next) {
   res.render('pages/dashboard', { title: 'dashboards' })
 })
 
-router.get('/workspace', function(req, res, next) {
+router.get('/workspace', function (req, res, next) {
   res.render('pages/workspace', { title: 'workspace' })
 })
 
 
-router.get('/market', function(req, res, next) {
+router.get('/market', function (req, res, next) {
   res.render('pages/market', { title: 'market' })
 })
-router.get('/marketDetail', function(req, res, next) {
+router.get('/marketDetail', function (req, res, next) {
   res.render('pages/marketDetail', { title: 'marketDetail' })
 })
 
-router.get('/presentation', function(req, res, next) {
+router.get('/presentation', function (req, res, next) {
   res.render('pages/presentation', { layout: 'layout_presentation' })
 })
 
